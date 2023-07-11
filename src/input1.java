99fc1e4c435a7acc6c28af735783a7809d0b03d2
Vishal Singh <vishalsinghpanwar02@gmail.com>
2023-07-11 13:15:48 +0530

    input1 done

--- src/input1.java
+++ src/input1.java
@@ -9,7 +9,6 @@ import javax.xml.parsers.ParserConfigurationException;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
-import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
@@ -30,6 +29,7 @@ public class input1 {
                 System.out.println("ItemDescription: " + item.getAttribute("ItemDescription"));
                 System.out.println("UOM: " + item.getAttribute("UOM"));
                 System.out.println("UnitPrice: " + item.getAttribute("UnitPrice"));
+                System.out.println(" ");
             }
         }
     }
